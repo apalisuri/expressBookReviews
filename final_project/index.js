f7e51f097cdcee43f0841f9f28c680a169dbcd54
apalisuri <119745652+apalisuri@users.noreply.github.com>
2023-01-05 09:16:43 -0700

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,8 +11,9 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
+//Write the authenication mechanism here
     if(req.session.authorization) {
-        let token = req.session.authorization['accessToken'];
+        token = req.session.authorization['accessToken'];
         jwt.verify(token, "access",(err,user)=>{
             if(!err){
                 req.user = user;
@@ -21,10 +22,10 @@ app.use("/customer/auth/*", function auth(req,res,next){
             else{
                 return res.status(403).json({message: "User not authenticated"})
             }
-         });
-     } else {
-         return res.status(403).json({message: "User not logged in"})
-     }
+        });
+    } else {
+        return res.status(403).json({message: "User not logged in"})
+    }
 });
  
 const PORT =5000;
